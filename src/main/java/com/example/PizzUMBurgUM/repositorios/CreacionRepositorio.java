f2f9f4dd5584c9acd64a52d6c4815507d6f9e587
Mateo Laco <mateolaco06@gmail.com>
2025-10-13 16:15:40 -0300

    Creé la clase de Creacion pero no tengo mucha idea de que hacer con eso

---
+++ src/main/java/com/example/PizzUMBurgUM/repositorios/CreacionRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.PizzUMBurgUM.repositorios;
+
+import com.example.PizzUMBurgUM.entidades.Creacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreacionRepositorio extends JpaRepository<Creacion, Integer> {
+}
